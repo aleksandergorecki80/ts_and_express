ab76ca7124076c4f84291c3fcdb1716bc085819b
Aleksander <aleksandergorecki80@gmail.com>
2022-08-18 00:05:37 +0200

    FOLD

--- src/app.ts
+++ src/app.ts
@@ -1,93 +1,35 @@
 import express, { Application, Request, Response, NextFunction } from 'express'
-
-import { Option, none, some } from 'fp-ts/Option'
-
-
-import { pipe, flow } from 'fp-ts/lib/function'
-import * as O from "fp-ts/Option"
-
-
-
-
-//      Map
-
-
-const foo = {
-  bar: 'hello',
-}
-
-const mapPipe =  pipe(foo, (f) => f.bar)
-console.log(mapPipe, ' = mapPipe')
-
-interface Foofoo {
-  bar: string | undefined
-}
-
-const foofoo: Foofoo  = {
-  bar: 'hello',
-} 
-
-const fofoPipe =  pipe(foofoo, (f) => f?.bar) // hello
-console.log(fofoPipe, ' = fofoPipe')
-
-const fofoPipeObj =  pipe(foofoo, ({bar}) => bar) // hello
-console.log(fofoPipeObj, ' = fofoPipeObj')
-
-// const fooNulable = pipe(
-//   foo,
-//   O.fromNullable,
-//   O.map(({ bar: { buzz } }) => buzz),
-// )
-
-// console.log(fooNulable, ' = fooNulable')
-
-interface Fizz {
-  buzz: string
-}
-
-interface Doo {
-  bar?: Fizz
-}
-
-const doo = { bar: undefined } as Doo | undefined
-
-pipe(doo, (f) => f?.bar?.buzz) // undefined
-
-
-const nulable1 =  pipe(
-  foo,
-  O.fromNullable,
-  O.map(({ bar }) => bar),
-) // { _tag: 'Some', value: 'hello' }
-
-const nulable2 = pipe(
-  undefined,
-  O.fromNullable,
-  O.map(({ bar }) => bar),
-) // { _tag: 'None' }
-
-
-console.log(nulable1, ' = nulable1')
-console.log(nulable2, ' = nulable2')
-
-
-
-
-
-const examplePipe = (a:any, b:any, c:any) => c(b(a))
-const res =  examplePipe(
-  1,
-  (x:any) => x + 1,
-  (x:any) => x + 5
-)
-
-console.log(res)
-
-
-
-
+import { fold } from "fp-ts/lib/Either"
 
 const app: Application = express()
+
+
+// app.use(bodyParser.json()) //enables json body deserializer
+app.post("/signin", handleSignIn)
+
+
+const signIn = (email: string, password: string) => {
+  return email && password ? 'logged in ' : 'new Error()'
+}
+
+function replyUnauthorized(res: Response): (error: Error) => void {
+  return error => res.status(401).end()
+}
+
+function replyToken(res: Response): (token: string) => void {
+  return token => res.json({ token })
+}
+
+// It can be read as 
+// "If signIn returns an error, reply with unauthorized response, otherwise, reply with a token". 
+
+function handleSignIn(req: Request, res: Response) {
+  const { email, password } = req.body
+  fold(replyUnauthorized(res), replyToken(res))(signIn('email', 'password'))
+}
+
+
+
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('Hello')
